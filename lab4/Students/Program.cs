9d80552b9562b373be27430b2e4756848a7c6cf7
KseniyaEvans <lahman.kseniya@gmail.com>
2020-03-21 12:43:24 +0200

    change strategy pattern to template methon

--- lab4/Students/Program.cs
+++ lab4/Students/Program.cs
@@ -3,84 +3,6 @@ using System.Collections.Generic;
 
 namespace Students
 {
-    // public enum Debt{
-    //     Educational,
-    //     Financial
-    // }; 
-
-    // public enum Parameter{
-    //     Fullname,
-    //     EducationalDebt,
-    //     FinancialDebt
-    // };
-    // 
-    // // abstract class IStudentStrategy
-    // // {
-    // //     // double CalculateTax(double amount);
-    // //     List<Student> _students = new List<Student>();
-    // //     public enum Debt{
-    // //         Educational,
-    // //         Financial
-    // //     }; 
-
-    // //     public enum Parameter{
-    // //         Fullname,
-    // //         EducationalDebt,
-    // //         FinancialDebt
-    // //     }; 
-    // //     public abstract void MakeStudentsList(List<Student> students, Debt debt);
-    // //     public abstract void SortStudetnsList(List<Student> students, Parameter param);
-    // //     public abstract void PrintStudentsList();
-    // // }
-
-
-    // class SortedList : IStudetns
-    // {
-    //     List<Student> _st = new List<Student>();
-    //     public List<Student> MakeStudentsList(List<Student> students, Parameter param)
-    //     {
-    //         switch (param)
-    //         {
-    //             case Parameter.Fullname: {
-    //                 SortByFullname(students);
-    //                 break;
-    //             }
-    //             case Parameter.EducationalDebt: {
-    //                 SortByEducationalDebt(students);
-    //                 break;
-    //             }
-    //             case Parameter.FinancialDebt: {
-    //                 SortByEducationalDebt(students);
-    //                 break;
-    //             }
-    //             default: {
-    //                 Console.WriteLine("ERROR");
-    //                 break;
-    //             }
-    //         }
-    //         return _st;
-    //     }
-    //     void makeEducationDebtList(List<Student> students)
-    //     {
-    //         foreach(Student st in students)
-    //         {
-    //             if (st.GetStudentEduDebt() != 0)
-    //             {
-    //                 this._st.Add(st);
-    //             }
-    //         }
-    //     }
-    //     void makeFinancialDebtList(List<Student> students)
-    //     {
-    //         foreach(Student st in students)
-    //         {
-    //             if (st.GetStudentFinDebt() != 0)
-    //             {
-    //                 this._st.Add(st);
-    //             }
-    //         }
-    //     }
-    // }
     class MainApp
     {
         static void Main()
@@ -96,48 +18,10 @@ namespace Students
             } ;
             StudentsList students_a = new StudentsList(curstudents);
             StudentsList students_b = new StudentsList(curstudents);
-            StudentsList students_c = new StudentsList(curstudents);
 
-            students_a.SetMakeListStrategy(new MakeListByEducationDebt());
-            students_a.SetSortStrategy(new SortByFullname());
-            students_a.SetPrintStrategy(new PrintWholeList());
+            students_a.GetResult(new MakeListByEducationDebt(), new SortByFullname(), new PrintWholeList());
+            students_b.GetResult(new MakeListByFinanceDebt(), new SortByFinanceDebt(), new PrintListByNameAndFinDebt());
 
-            Console.WriteLine("List at the beginning: ");
-            students_a.Print();
-
-            Console.WriteLine("Maked by Education Debt list: ");
-            students_a.Make();
-            students_a.Print();
-
-            Console.WriteLine("Sorted by fullname list and printed only by fullname: ");
-            students_a.SetPrintStrategy(new PrintListByName());
-            students_a.Sort();
-            students_a.Print();
-
-            Console.WriteLine("Sorted by education debt list: ");
-            students_a.SetSortStrategy(new SortByEducationDebt());
-            students_a.SetPrintStrategy(new PrintListByNameAndEduDebt());
-            students_a.Sort();
-            students_a.Print();
-
-            Console.WriteLine("Sorted by finance debt list: ");
-            students_a.SetSortStrategy(new SortByFinanceDebt());
-            students_a.SetPrintStrategy(new PrintListByNameAndFinDebt());
-            students_a.Sort();
-            students_a.Print();
-            // students_b.Make();
-
-            
-            // studentRecords.SetSortStrategy(new SortByFullname());
-            // studentRecords.Sort();
-
-            // studentRecords.SetSortStrategy(new SortByEducationDebt());
-            // studentRecords.Sort();
-
-            // studentRecords.SetSortStrategy(new SortByFinanceDebt());
-            // studentRecords.Sort();
-
-            // Wait for user
             Console.ReadKey();
         }
     }
@@ -149,36 +33,34 @@ namespace Students
         private MakeListStrategy _makeliststrategy;
         private PrintStrategy _printstrategy;
 
+        public void GetResult(MakeListStrategy makestrategy, SortStrategy sortstrategy, PrintStrategy printstrategy)
+        {
+            this._makeliststrategy = makestrategy;
+            this._sortstrategy = sortstrategy;
+            this._printstrategy = printstrategy;
+
+            this.Make();
+            this.Sort();
+            this.Print();
+
+            Console.WriteLine("Successfully done!\n");
+
+        }
         public StudentsList(List<Student> students)
         {
             this._list = students;
         }
-        public void SetSortStrategy(SortStrategy sortstrategy)
-        {
-            this._sortstrategy = sortstrategy;
-        }
-
-        public void SetMakeListStrategy(MakeListStrategy makestrategy)
-        {
-            this._makeliststrategy = makestrategy;
-        }
-
-        public void SetPrintStrategy(PrintStrategy printstrategy)
-        {
-            this._printstrategy = printstrategy;
-        }
-        public void Sort()
+        private void Sort()
         {
             _sortstrategy.Sort(_list);
         }
-        public void Make()
+        private void Make()
         {
             this._list = _makeliststrategy.MakeList(_list);   
         }
-        public void Print()
+        private void Print()
         {
             _printstrategy.PrintList(_list);
         }
     }
-
 }
